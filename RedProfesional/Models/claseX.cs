14a87f5725c1e1e3a84d4b7089bf77a9086f1332
joseluissimeon777 <joseluissimeon777@DESKTOP-IHB3PF3>
2018-05-24 10:46:36 -0300

    Agrego De clase x

---
+++ RedProfesional/Models/claseX.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RedProfesional.Models
+{
+    public class claseX
+    {
+
+        public string PropX { get; set; }
+    }
+}
