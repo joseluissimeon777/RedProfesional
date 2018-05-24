6517265bf43c7ad18c22cb1b15490a5b21ab873f
joseluissimeon777 <joseluissimeon777@DESKTOP-IHB3PF3>
2018-05-24 10:30:18 -0300

    Agregar archivos de proyecto.

---
+++ RedProfesional/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RedProfesional.Startup))]
+namespace RedProfesional
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
